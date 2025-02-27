8a87544cee9818601eda29f18a3c730500df986d
PrakharGupta04 <148334840+PrakharGupta04@users.noreply.github.com>
2025-02-27 18:57:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const _dirname = path.resolve()
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://murli-mustard-oil-two.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
